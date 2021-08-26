326f0aa2d301d4656c6369dbe86e551db8790a9d
FrankieNay <89489233+FrankieNay@users.noreply.github.com>
2021-08-26 16:33:27 -0500

    Activity 1-3 Complete

--- Crab.java
+++ Crab.java
@@ -1,14 +1,59 @@
-import greenfoot.*;
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * This class defines a crab. Crabs live on the beach.
+ * Write a description of class Crab here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
  */
 public class Crab extends Actor
 {
+    /**
+     * Act - do whatever the Crab wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
     public void act()
     {
-        // Add your action code here
+        move(3);
+        turnAtEdge();
+        checkKeyPress();
+        onCollision();
+    }
+
+private void turnAtEdge()
+{
+    if(isAtEdge())
+    {
+        turn(50);
     }
 }
 
+private void checkKeyPress()
+{
+    if(Greenfoot.isKeyDown("right"))
+    {
+        turn(4);
+    }
 
+if(Greenfoot.isKeyDown("left"))
+{
+    turn(-4);
+}
+}
+
+private void onCollision()
+{
+    if(isTouching(Worm.class))
+    {
+    removeTouching(Worm.class);
+    Greenfoot.playSound("slurp.wav");
+}
+
+}
+
+
+
+
+
+
+}
--- CrabWorld.java
+++ CrabWorld.java
@@ -9,5 +9,32 @@ public class CrabWorld extends World
     public CrabWorld() 
     {
         super(560, 560, 1);
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Crab crab = new Crab();
+        addObject(crab,225,177);
+        Worm worm = new Worm();
+        addObject(worm,351,260);
+        Worm worm2 = new Worm();
+        addObject(worm2,369,431);
+        Worm worm3 = new Worm();
+        addObject(worm3,403,42);
+        Worm worm4 = new Worm();
+        addObject(worm4,167,416);
+        Worm worm5 = new Worm();
+        addObject(worm5,474,299);
+        Worm worm6 = new Worm();
+        addObject(worm6,172,65);
+        Worm worm7 = new Worm();
+        addObject(worm7,145,298);
+        Worm worm8 = new Worm();
+        addObject(worm8,431,149);
     }
 }
